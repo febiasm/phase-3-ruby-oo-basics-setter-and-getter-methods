331e1d7a3e419e11c1e0737dc0142df755bc30b6
febiasm <trevorfebias@gmail.com>
2023-02-15 15:24:30 +0300

    done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,15 @@
-class Dog
+ class Dog
+    def name=(name)
+        @name = name
+    end
+    def breed=(breed)
+        @breed = breed
+    end
+    def name
+        @name
+    end
+    def breed
+        @breed
+    end
 
-  def name=(name)
-      @name = name
-  end
-  def name
-      @name
-  end
-  def breed=(breed)
-      @breed = breed
-  end
-  def breed
-      @breed
-  end
-end
+ end
